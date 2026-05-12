14bd6e5982a6f24f335052e94cab4c67b3ad0ba2
Lucas Dias <ld.ggd@outlook.com>
2026-05-11 21:51:25 -0300

    [TEST]

--- src/app/pokemon-list/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-list/pokemon-card/pokemon-card.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PokemonNamePipe } from '../../shared/pipes/pokemon-name-pipe';
+import { PokemonListService } from '../pokemon-list.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -8,6 +9,14 @@ import { PokemonNamePipe } from '../../shared/pipes/pokemon-name-pipe';
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.css',
 })
-export class PokemonCardComponent {
+export class PokemonCardComponent implements OnInit {
   @Input(`selectedPokemon`) selectedPokemon: any = null;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(`passou aaa`);
+  }
 }
--- src/app/pokemon-list/pokemon-list.component.spec.ts
+++ src/app/pokemon-list/pokemon-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonListComponent } from './pokemon-list.component';
-import { PokemonListService } from './pokemon-list.service';
-import { of } from 'rxjs';
-import { By } from '@angular/platform-browser';
-import { getPokemonByNameMock } from '../mock/pokemon-list/getPokemonByName';
-import { getPokemonListMock } from '../mock/pokemon-list/getPokemonList';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { By } from '@angular/platform-browser';
+import { getPokemonByNameMock } from '../mock/pokemon-list/getPokemonByName';
+import { getPokemonListMock } from '../mock/pokemon-list/getPokemonList';
+import { PokemonListService } from './pokemon-list.service';
+import { of } from 'rxjs';
 import { MockComponent } from 'ng-mocks';
 
-const mockService = {
+const serviceMock = {
   getPokemonList: vi.fn().mockReturnValue(of(getPokemonListMock)),
   getPokemonByName: vi.fn().mockReturnValue(of(getPokemonByNameMock)),
 };
@@ -25,23 +25,17 @@ describe('PokemonListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        CommonModule,
-        MatSelectModule,
-        ReactiveFormsModule,
-        MatInputModule,
-        MatButtonModule,
-        MockComponent(PokemonCardComponent),
-      ],
+      imports: [PokemonListComponent, MockComponent(PokemonCardComponent)],
       providers: [
         {
           provide: PokemonListService,
-          useValue: mockService,
+          useValue: serviceMock,
         },
       ],
+      declarations: [PokemonListComponent],
     }).compileComponents();
 
-    vi.clearAllMocks();
+    vi.restoreAllMocks();
 
     fixture = TestBed.createComponent(PokemonListComponent);
     component = fixture.componentInstance;
@@ -52,76 +46,24 @@ describe('PokemonListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should get initial pokemons data', () => {
-    expect(component.pokemonList.length).toBe(getPokemonListMock.results.length);
-  });
-
-  it('Should have mat option of data of pokemonListMock', () => {
-    const trigger = fixture.debugElement.query(By.css('.mat-mdc-select-trigger'));
-    trigger.triggerEventHandler('click');
-    fixture.detectChanges();
-
-    const matOptionList = fixture.debugElement.queryAll(By.css('mat-option > span'));
-    expect(matOptionList.length).toBe(getPokemonListMock.results.length);
-
-    const firstOptionIndex = 0;
-    const secondOptionIndex = 6;
-
-    const firstOption = matOptionList[firstOptionIndex].nativeElement as HTMLOptionElement;
-    const secondOption = matOptionList[secondOptionIndex].nativeElement as HTMLOptionElement;
-
-    expect(firstOption.textContent).toBe(getPokemonListMock.results[firstOptionIndex].name);
-    expect(secondOption.textContent).toBe(getPokemonListMock.results[secondOptionIndex].name);
-  });
-
-  it('Should select a mat option and change control value', () => {
-    const trigger = fixture.debugElement.query(By.css('.mat-mdc-select-trigger'));
-    trigger.triggerEventHandler('click');
-    fixture.detectChanges();
-
-    const matOptionList = fixture.debugElement.queryAll(By.css('mat-option'));
-
-    const optionClicked = 0;
-
-    matOptionList[optionClicked].triggerEventHandler('click');
-    expect(component.selectedPokemonControl.value).toBe(
-      getPokemonListMock.results[optionClicked].name,
-    );
-  });
-
-  it('Test if input is working', () => {
-    const inputValue = 'gengar';
+  it('Input control value', () => {
+    const valorDeTest = `teste`;
     const input = fixture.debugElement.query(By.css('input')).nativeElement as HTMLInputElement;
-    input.value = inputValue;
-    input.dispatchEvent(new Event('input'));
+    input.value = valorDeTest;
+    input.dispatchEvent(new Event(`input`));
+
     fixture.detectChanges();
 
-    expect(component.pokemonSearchControl.value).toBe(inputValue);
+    expect(component.pokemonSearchControl.value).toBe(valorDeTest);
   });
 
-  it('Test if search of input is working', () => {
-    expect(mockService.getPokemonByName).toHaveBeenCalledTimes(0);
-
-    const inputValue = 'gengar';
-    component.pokemonSearchControl.setValue(inputValue);
-
-    const button = fixture.debugElement.query(By.css('button'));
-    button.triggerEventHandler('click');
-
-    expect(mockService.getPokemonByName).toHaveBeenCalledTimes(1);
-    expect(mockService.getPokemonByName).toHaveBeenCalledWith(inputValue);
-  });
-
-  it('Validate pokemon-card input', () => {
-    component.selectedPokemonControl.setValue('gengar');
-    component.selected();
+  it('Button click trigger search', () => {
+    expect(component.selectedPokemon()).toBeNull();
+    expect(serviceMock.getPokemonByName).toHaveBeenCalledTimes(0);
+    const button = fixture.debugElement.query(By.css(`button`)).nativeElement as HTMLButtonElement;
+    button.dispatchEvent(new Event(`click`));
     fixture.detectChanges();
-
-    const cardMock = fixture.debugElement.query(By.css('app-pokemon-card'));
-    const cardComponent = cardMock.componentInstance;
-
-    console.log(cardComponent.loading);
-
-    expect(cardComponent.selectedPokemon).toBe(component.selectedPokemon());
+    expect(serviceMock.getPokemonByName).toHaveBeenCalledTimes(1);
+    expect(component.selectedPokemon()).toBe(getPokemonByNameMock);
   });
 });
