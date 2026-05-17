95ba116ffaa359f6f747b87e5c48932ba56188ff
Lucas Dias <ld.ggd@outlook.com>
2026-05-17 00:26:05 -0300

    dropdown transition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Template2Component } from './template2/template2.component';
 import { Template3Component } from './template3/template3.component';
 import { TableTestComponent } from './table-test/table-test.component';
 import { CustomInputComponent } from './custom-input/custom-input.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [{ path: '**', component: CustomInputComponent }];
+export const routes: Routes = [{ path: '**', component: NavbarComponent }];
